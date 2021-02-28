d25961d71cb01f783b338dd56aaaf5bf836eb14b
unknown <samiazahan3@gmail.com>
2021-02-28 21:18:10 +0600

    first commit

---
+++ EventManegement/EventManegement/Models/Clients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EventManegement.Models
+   
+{
+    public class Clients
+    {
+        public int C_Id { get; set; }
+        public string C_Add { get; set; }
+        public string C_Email { get; set; }
+        public string C_Phone { get; set; }
+        public string C_Pass { get; set; }
+        public string C_Username { get; set; }
+        public string C_Name { get; set; }
+
+    }
+    public class ClientsDBContext : Clients
+    {
+        public  Dbset<Clients> Client {get; set;}
+    }
+}
+   
